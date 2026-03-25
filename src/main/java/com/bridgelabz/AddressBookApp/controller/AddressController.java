801a383827764b4acc3326426fd0a402e888a9f6
Maha <maharagav20@gmail.com>
2026-03-25 13:01:54 +0530

    [maha]: UC4: AddressBook Application Settings

--- src/main/java/com/bridgelabz/AddressBookApp/controller/AddressController.java
+++ src/main/java/com/bridgelabz/AddressBookApp/controller/AddressController.java
@@ -1,46 +1,49 @@
 package com.bridgelabz.AddressBookApp.controller;
+
 import com.bridgelabz.AddressBookApp.dto.AddressDTO;
 import com.bridgelabz.AddressBookApp.service.AddressService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
-
-
-
-
 @RequestMapping("/addressbook")
 public class AddressController {
 
-    private List<AddressDTO> list = new ArrayList<>();
     @Autowired
     AddressService service;
+
     // GET ALL
     @GetMapping("/")
     public List<AddressDTO> getAll() {
+        log.info("Fetching all contacts");
         return service.getAll();
     }
 
     @PostMapping("/create")
     public AddressDTO create(@RequestBody AddressDTO dto) {
+        log.info("Creating contact: {}", dto);
         return service.create(dto);
     }
 
     @GetMapping("/get/{id}")
     public AddressDTO getById(@PathVariable int id) {
+        log.info("Fetching contact with id: {}", id);
         return service.getById(id);
     }
 
     @PutMapping("/update/{id}")
     public AddressDTO update(@PathVariable int id, @RequestBody AddressDTO dto) {
+        log.info("Updating contact with id: {}", id);
         return service.update(id, dto);
     }
 
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable int id) {
+        log.info("Deleting contact with id: {}", id);
         service.delete(id);
         return "Deleted";
     }
