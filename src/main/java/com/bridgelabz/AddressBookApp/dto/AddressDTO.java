fc9f179ecdfca7c9f4a389e43abded6519d75247
Maha <maharagav20@gmail.com>
2026-03-25 14:10:58 +0530

    [maha]: UC5: AddressBook Validation and Exception Handling

--- src/main/java/com/bridgelabz/AddressBookApp/dto/AddressDTO.java
+++ src/main/java/com/bridgelabz/AddressBookApp/dto/AddressDTO.java
@@ -1,10 +1,17 @@
 package com.bridgelabz.AddressBookApp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AddressDTO {
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Invalid name")
     private String name;
+
     private String address;
+
     private String phone;
 }
